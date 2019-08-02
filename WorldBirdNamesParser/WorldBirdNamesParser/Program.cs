387b6fe867d2e56f3c2de1a8646a84880fae0d76
Andreas <thecodingbeast@gmail.com>
2019-08-02 18:09:29 +0200

    Added excel file opening and reading

--- WorldBirdNamesParser/WorldBirdNamesParser/Program.cs
+++ WorldBirdNamesParser/WorldBirdNamesParser/Program.cs
@@ -1,21 +1,36 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using Microsoft.Office.Interop.Excel;
 
 namespace WorldBirdNamesParser
 {
     class Program
     {
+        static readonly string file = "demo.xlsx";
+        static readonly string path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            // Create new excel application and open file.
+            Application excel = new Application();
+            Workbook workbook = excel.Workbooks.Open(Path.Combine(path, file));
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            // Get sheet range and loop through rows and columns.
+            Range range = workbook.Sheets[1].UsedRange;
+            for (int row = 1; row <= range.Rows.Count; row++)
+            {
+                for (int col = 1; col <= range.Columns.Count; col++)
+                {
+                    // Skip if cell is empty.
+                    if (range.Cells[row, col].Value2 == null) continue;
+
+                    // Output cell value to console.
+                    Console.WriteLine(range.Cells[row, col].Value2);
+                }
+            }
+
+            // Wait for user input.
+            Console.ReadKey();
         }
     }
 }
